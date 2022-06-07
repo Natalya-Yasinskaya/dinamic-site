f999bc227d3d8f60e33db8d130b11ebc16b1d16c
Natalya Yasinskaya <Natalya.ponomarenko.94@mail.ru>
2022-06-07 17:00:41 +0300

    Add files via upload

---
+++ log.php
@@ -0,0 +1,73 @@
+<?php   include("path.php");
+        include SITE_ROOT . "/app/controllers/users.php";
+?>
+
+<!doctype html>
+<html lang="ru">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    
+    <!-- Font Awesome -->
+    <script src="https://kit.fontawesome.com/8b3408cd41.js" crossorigin="anonymous"></script>
+
+    <!-- Custom Styling -->
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Comfortaa:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+
+    <title>My blog</title>
+  </head>
+  <body>
+    
+  <?php include("app/include/header.php"); ?>
+
+    <!--Form START-->
+    <div class="container reg_form">
+        <form class="row justify-content-center" method="post" action="log.html">
+            <h2 class="col-12">Авторизация</h2>
+            <div class="mb-3 col-12 col-md-4 err">
+                <p><?=$errMsg[0]?></p>
+            </div>
+            <div class="w-100"></div>
+            <div class="mb-3 col-12 col-md-4">
+                <label for="formGroupExampleInput" class="form-label">Ваш email</label>
+                <input name="mail" value="<?=$email?>" type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="введите ваш email...">
+            </div> 
+            <div class="w-100"></div>
+            <div class="mb-3 col-12 col-md-4">
+            <label for="exampleInputPassword1" class="form-label">Пароль</label>
+            <input name="password" type="password" class="form-control" id="exampleInputPassword1">
+            </div>
+            <div class="w-100"></div>
+            <div class="mb-3 col-12 col-md-4">
+                <button type="submit" class="btn btn-secondary">Войти</button>
+                <a href="<?php echo BASE_URL . "reg.php"; ?>">Зарегистрироваться</a>
+            </div>
+        </form>
+    </div>
+    <!--Form END-->
+
+
+
+    <!--footer start-->
+    <?php include("app/include/footer.php"); ?>
+    <!--footer end-->
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+    -->
+  </body>
+</html>
